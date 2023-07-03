c6ae72f15601f7e82f12a634eccbce333f61b4c4
Vlad Fokin <fokin.vladyslav@gmail.com>
2023-07-03 22:32:37 +0300

    Lesson 2.2

---
+++ Lesson_2.2/2.py
@@ -0,0 +1,39 @@
+# a = 3.0
+# b = 5.0
+# c = (a**2 + b**2) ** 0.5
+
+# print ("Гипотенуза", c)
+
+# # x = x * 2
+
+
+# a = input("tell_my")
+# print("what", a)
+
+# num1 = float(input("1 number"))
+# num2 = float(input("2 number:"))
+# num3 = float(input("3 number:"))
+
+# 1.
+# # a=  num1 + num2 + num3
+# # b= num1 * num2 * num3 
+# # print("Сумма",a)
+# # print("Умножение", b)
+
+# 2.
+# sur_name = input("Fam")
+# name = input("Name")
+
+# print ("Hi on good program:", sur_name,name)
+# print(sur_name)
+# print(name)
+
+3.
+metr = float((input("Metr:" )))
+
+kilometr = (metr/1000)
+milimetr = (metr*1000)
+
+print (metr, "m")
+print (kilometr, "km")
+print (milimetr, "mm")
